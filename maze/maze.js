5c30767f65f8ce4f0d3afe694ea818ee5c84dafb
Bipin Karki <bipinkarki172@gmail.com>
2022-07-30 09:10:26 -0400

    assignment 8: jquery maze

---
+++ maze/maze.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $("#start").click(function () {
+        let start = true;
+        $("#maze .boundary").removeClass("youlose");
+        $("#status").text("Move mouse to the end without touching boundary in order to win the game");
+        $("#maze .boundary").mouseover(function () {
+            updateboundary();
+        });
+        function updateboundary(){
+            if(start){
+                $("#maze .boundary").addClass("youlose");
+                $("#status").text("You Lose!!!!");
+            }
+        }
+        $("#end").mouseenter(function () {
+            if (!$(".boundary").hasClass("youlose")) {
+                $("#status").text("Congratulation!! You Win");
+                start = false;
+            }
+        })
+        $("#maze").mouseleave(function () {
+            updateboundary();
+        })
+    });
+})
